5801a5882817d90bc52e27112cbf565586ce7e46
MinaRamsisASD1993 <minaramsis1993@outlook.com>
2019-01-18 23:24:26 +0200

    Updates

---
+++ precache-manifest.6be727d5d57e8f4572b9a334bb7f109d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9629e9ac880cca81bb2f",
+    "url": "/reactContactManager/static/js/runtime~main.9629e9ac.js"
+  },
+  {
+    "revision": "3517343db532e5ef6e97",
+    "url": "/reactContactManager/static/js/main.3517343d.chunk.js"
+  },
+  {
+    "revision": "8cfd5f4d97b8e83ff08a",
+    "url": "/reactContactManager/static/js/1.8cfd5f4d.chunk.js"
+  },
+  {
+    "revision": "3517343db532e5ef6e97",
+    "url": "/reactContactManager/static/css/main.d529bb66.chunk.css"
+  },
+  {
+    "revision": "8cfd5f4d97b8e83ff08a",
+    "url": "/reactContactManager/static/css/1.382c0f0b.chunk.css"
+  },
+  {
+    "revision": "cf12b4916f6674cfe9a1ec87562e9c0d",
+    "url": "/reactContactManager/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/reactcontactmanager/precache-manifest.8b7ebc16bb5ad8fd73f095aa1f05c7d8.js"
+  "/reactContactManager/precache-manifest.6be727d5d57e8f4572b9a334bb7f109d.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/reactcontactmanager/index.html", {
+workbox.routing.registerNavigationRoute("/reactContactManager/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
